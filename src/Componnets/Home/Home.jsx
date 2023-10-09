d90b07fee6b05939b9bfc3520d29701e22eab342
SALAB\dellv <salabyaham@gmail.com>
2023-10-09 08:13:42 +0600

    animation has been added

--- src/Componnets/Home/Home.jsx
+++ src/Componnets/Home/Home.jsx
@@ -6,27 +6,37 @@ import Service from "../Service/Service";
 import { useEffect } from "react";
 import Testinomials from "../Testinomials/Testinomials";
 import Gallary from "../Gallary/Gallary";
+import Aos from "aos";
+import 'aos/dist/aos.css'
+
 
 
 const Home = () => {
-
-const services =useLoaderData()
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
+    const services = useLoaderData()
     return (
         <div>
             <Navbar></Navbar>
-            <Bannar></Bannar>
+            <div data-aos="fade left">
+                <Bannar></Bannar>
+            </div>
             <h1 className="text-center p-4 text-3xl border-b-4 border-[#008080] font-semibold text-zinc-800">Our services</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> 
-      
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" data-aos="fade-right">
+
                 {
                     services.map(service => <Service key={service.id} service={service}></Service>)
                 }
             </div>
-            <h1 className="text-center p-4 text-3xl border-b-4 border-[#008080] font-semibold text-zinc-800 mt-6">What Clients Say !</h1>
-            <Testinomials></Testinomials>
-
-            <h1 className="text-center p-4 text-3xl border-b-4 border-[#008080] font-semibold text-zinc-800 mt-6">Gallary </h1>
-            <Gallary></Gallary>
+            <div data-aos="fade-up">
+                <h1 className="text-center p-4 text-3xl border-b-4 border-[#008080] font-semibold text-zinc-800 mt-6">What Clients Say !</h1>
+                <Testinomials></Testinomials>
+            </div>
+            <div data-aos="fade-in">
+                <h1 className="text-center p-4 text-3xl border-b-4 border-[#008080] font-semibold text-zinc-800 mt-6">Gallary </h1>
+                <Gallary></Gallary>
+            </div>
 
             <Footer></Footer>
         </div>
